fc513b61443823dad8fff8e8156cd977c5e6c28b
Valuev Semyon <valuevs466@gmail.com>
2024-06-03 01:27:54 +0300

    изменения по ртранзакциям и роллбекам

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -17,7 +17,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         // Создает таблицу users в вашей базе данных.
-        try (Statement statement = connection.createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
             connection.setAutoCommit(false);
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS User " +
                     "(id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45), lastName VARCHAR(45), age TINYINT)");
@@ -29,12 +30,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 throw new RuntimeException(ex);
             }
             System.err.println("Ошибка при создании таблицы users: " + e.getMessage());
+        }finally {
+            if (connection != null) {}
         }
     }
 
     public void dropUsersTable() {
         // УДАЛЯЕТ ТАБЛИЦУ ИЗ БАЗЫ ДАННЫХ
-        try (Statement statement = connection.createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
             connection.setAutoCommit(false);
             statement.executeUpdate("DROP TABLE IF EXISTS User");
             connection.commit();
@@ -45,13 +49,16 @@ public class UserDaoJDBCImpl implements UserDao {
                 throw new RuntimeException(ex);
             }
             System.err.println("Ошибка при удалении таблицы: " + e.getMessage());
+        }finally {
+            if (connection != null) {}
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         // Вставляет новую запись пользователя в таблицу users.
-        try (PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO User (name, lastName, age) VALUES (?, ?, ?)")) {
+        try {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "INSERT INTO User (name, lastName, age) VALUES (?, ?, ?)");
             connection.setAutoCommit(false);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
@@ -66,12 +73,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 throw new RuntimeException(ex);
             }
             System.err.println("Ошибка при сохранении  пользователя: " + e.getMessage());
+        }finally {
+            if (connection != null) {}
         }
     }
 
     public void removeUserById(long id) {
         // Удаляет запись пользователя по его ID из таблицы users.
-        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM User WHERE id = ?")) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM User WHERE id = ?");
             connection.setAutoCommit(false);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
@@ -83,12 +93,16 @@ public class UserDaoJDBCImpl implements UserDao {
                 throw new RuntimeException(ex);
             }
             System.err.println("Ошибка при удалении: " + e.getMessage());
+        }finally {
+            if (connection != null) {}
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
             ResultSet resultSet = statement.executeQuery("SELECT * FROM User");
             while (resultSet.next()) {
                 User user = new User();
@@ -97,16 +111,25 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setLastName(resultSet.getString("lastName"));
                 user.setAge(resultSet.getByte("age"));
                 users.add(user);
+                connection.commit();
             }
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
             e.printStackTrace();
+        }finally {
+            if (connection != null) {}
         }
         return users;
     }
 
     public void cleanUsersTable() {
         // Очищает таблицу users, удаляя все записи.
-        try (Statement statement = connection.createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
             connection.setAutoCommit(false);
             statement.execute("TRUNCATE TABLE User");
             connection.commit();
@@ -117,6 +140,8 @@ public class UserDaoJDBCImpl implements UserDao {
                 throw new RuntimeException(ex);
             }
             System.out.println("Таблица не очистилась: " + e.getMessage());
+        }finally {
+            if (connection != null) {}
         }
     }
 }
