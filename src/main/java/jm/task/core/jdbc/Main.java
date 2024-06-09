2170c26de7e8306d00c7743e4ea635df0e8df6ad
Valuev Semyon <valuevs466@gmail.com>
2024-06-09 23:20:08 +0300

    correction2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,25 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-        userDaoHibernate.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
 
-        userDaoHibernate.saveUser("Павел", "Валуев", (byte) 34);
-        userDaoHibernate.saveUser("Михаил", "Валуев", (byte) 64);
-        userDaoHibernate.saveUser("Максим", "Бусарев", (byte) 22);
-        userDaoHibernate.saveUser("Сергей", "Петров", (byte) 26);
+        userService.saveUser("Павел", "Валуев", (byte) 34);
+        userService.saveUser("Михаил", "Валуев", (byte) 64);
+        userService.saveUser("Максим", "Бусарев", (byte) 22);
+        userService.saveUser("Сергей", "Петров", (byte) 26);
 
-        System.out.println(userDaoHibernate.getAllUsers());
+        System.out.println(userService.getAllUsers());
 
-        userDaoHibernate.removeUserById(1);
-        userDaoHibernate.removeUserById(2);
-        userDaoHibernate.removeUserById(3);
-        userDaoHibernate.removeUserById(4);
+        userService.removeUserById(1);
+        userService.removeUserById(2);
+        userService.removeUserById(3);
+        userService.removeUserById(4);
 
-        userDaoHibernate.dropUsersTable();
+        userService.dropUsersTable();
     }
 }
